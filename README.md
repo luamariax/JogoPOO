2e71b4a0c50634395bd0829114d49f61ab84c946
Isabela Marcondes dos Santos <isabelamds00@gmail.com>
2026-06-07 21:50:02 -0300

    vou terminar de estudar pygame para terminar a tela jogo

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # JogoPOO
 
+# Versão Python = 3.11 (o mais estável para o pygame, o 3.14 ainda não tem suporte para o pygame)
+
 # Proposta do projeto + fases de implementação
 # https://docs.google.com/document/d/1p1R52JX-hDhVOb1TC9io85oZfDKJlZxOiOL-BLvFUEA/edit?usp=sharing
--- View/tela_jogo.py
+++ View/tela_jogo.py
@@ -1,2 +1,19 @@
 # Recebe o offset da câmera antes de desenhar qualquer coisa.
 # lê ComponentePosicao + ComponenteSprite → desenha na tela
+
+import pygame
+
+class TelaJogo:
+    def __init__(self):
+        pygame.init() #inicializa tudo do pygame
+        
+        info = pygame.display.Info() #pega informações da tela do usuário
+        self.largura = info.current_w
+        self.altura = info.current_h
+        
+        self.janela = pygame.display.set_mode((self.largura, self.altura)) #set_mode cria a janela com o tamanho da tela
+        pygame.display.set_caption("JOGOPOO") #define o título da janela
+        
+        self.clock = pygame.time.Clock() #controla o FPS, vai ser usado no loop principal
+
+        def desenhar(self, entidades):
