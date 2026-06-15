ebb0d9db60902264cb11f38fdda5817af12f60c4
Itamar UFMG <69490985+RamatiSeraos@users.noreply.github.com>
2026-06-15 17:51:29 -0300

    ajustei o jogador para ser 64 pixel

--- Model/jogador.py
+++ Model/jogador.py
@@ -8,7 +8,7 @@ from Model.Componentes.animacao import ComponenteAnimacao
 class Jogador(Entidade):
     def __init__(self, x: float, y: float):
         super().__init__()
-        self.adicionar_componente("posicao", ComponentePosicao(x, y, 30, 30)) #onde está e tamanho
+        self.adicionar_componente("posicao", ComponentePosicao(x, y, 64, 64)) #onde está e tamanho
         self.adicionar_componente("fisica", ComponenteFisica(gravidade=True, no_chao=False)) # para se mover
         # solido=False porque o jogador não bloqueia a passagem de outras entidades
         # tipo="normal" porque reage à física (não é gatilho nem causa dano por contato)
